6479ada4580dc82b72dc4e2a627c2b41b56ebc2a
Sajjad Siam <86528048+sajjadsiam@users.noreply.github.com>
2023-12-11 21:32:29 +0600

    Update and rename dsa lab 2.2.cpp to Quadratic Equation Solver.cpp

--- Quadratic Equation Solver.cpp
+++ Quadratic Equation Solver.cpp
@@ -6,20 +6,21 @@ using namespace std;
 int main() {
     double a, b, c, d, x1, x2, x;
     
+ 
+    cout << "Enter coefficients (a, b, c): ";
     cin >> a >> b >> c;
-    
+  
     d = (b * b - 4 * a * c);
-    
+
     if (d < 0) {
         cout << "No real solutions";
     }
-    
     else if (d > 0) {
+  
         x1 = (-b + sqrt(d)) / (2 * a);
         x2 = (-b - sqrt(d)) / (2 * a);
         cout << "x1: " << x1 << "    " << "x2: " << x2 << endl;
     }
-    
     else {
         x = (-b / (2 * a));
         cout << "x: " << x << endl;
